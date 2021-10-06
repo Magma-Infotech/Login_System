6d71fdf45ccdd56fb2c47b9dc8089fb683f1088e
Rajnish Singh <78335498+rajnish-488@users.noreply.github.com>
2021-10-06 16:23:17 +0530

    made the index.html page responsive

--- js/code.js
+++ js/code.js
@@ -1,1 +1,34 @@
-// the js code to be hear
+const navSlide = () =>{
+	const burger = document.querySelector('.toggle');
+	const nav=document.querySelector('.MenuItems');
+	const navLinks=document.querySelectorAll('.MenuItems li');
+	
+	burger.addEventListener('click', () =>{
+		// for toggle
+		nav.classList.toggle('nav-active');
+		// for nav-list
+		navLinks.forEach((link, index) => {
+			if(link.style.animation)
+			{
+				link.style.animation='';
+			} else {
+				link.style.animation = "navLinkFade 0.5s ease-in 0.5s forwards" ;
+			}
+		
+		});
+		
+	});
+	
+
+	
+
+
+}
+
+
+const app = () =>{
+	navSlide();
+}
+
+
+app();
